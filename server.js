09d591e72a0e72be9fe780241d8f4fbeac8d4a02
arnves <arnves@arnves.net>
2025-06-01 08:22:33 +0200

    Small modification

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 3000; // The port inside the container
 
 app.get('/', (req, res) => {
-  const appVersion = process.env.APP_VERSION || "1.0.0";
-  res.send(`Hello from QNAP! Version: ${appVersion} - Deployed at: ${new Date().toISOString()}`);
+  const appVersion = process.env.APP_VERSION || "1.0.1";
+  res.send(`Hello again from QNAP! Version: ${appVersion} - Deployed at: ${new Date().toISOString()}`);
 });
 
 app.listen(port, () => {
